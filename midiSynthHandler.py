73732c4b45e3b3fac58b9ad1b25aa90b89390e37
Bill S <will.staf@gmail.com>
2018-08-24 02:52:39 +0000

    script progress

--- midiSynthHandler.py
+++ midiSynthHandler.py
@@ -6,7 +6,7 @@ SoundFontFile = "/usr/share/sounds/sf2/drumSoundFonts/PremierKit.sf2"
 AiyBtnPin = 23
 AiyLedPin = 25
 
-def initFluidSynth(soundFonta):
+def initFluidSynth(soundFont):
     #start fluidsynth in the backround
     subprocess.Popen(["fluidsynth",
         "-is",
@@ -14,24 +14,65 @@ def initFluidSynth(soundFonta):
         "--gain=3",
         soundFont])
 
-def attaachMidiToSynth(inputChannel, outputChannel):
-    subprocess.Popen(["aconnect",
-        inputChannel+":0",
-        outputChannel+":0"])
+def stopFluidSynth():
+    subprocess.Popen(["sudo",
+        "pkill",
+        "fluidsynth"])
 
-def onMidiAttachBtn():
+def connectMidiToSynth(inputChannel, outputChannel):
+    try:
+        out = subprocess.check_output(["aconnect",
+            inputChannel+":0",
+            outputChannel+":0"])
+       # if out.contains("Connection Faild"):
+        #    return false
+    except:
+        return False
+    return True
+
+def disconnectMidiSynth():
+    subprocess.Popen(["aconnect",
+        "-x"]);
+
+def lightOn():
+    GPIO.output(AiyLedPin, True)
+
+def lightOff():
+    GPIO.output(AiyLedPin, False)
+
+def lightBlink(count):
+    for x in range(count):
+        GPIO.output(AiyLedPin, True)
+        time.sleep(0.234)
+        GPIO.output(AiyLedPin, False)
+        time.sleep(0.234)
+
+def onMidiAttachBtn(channel):
+    stopFluidSynth()
+    lightOff()
+    print("Start FluidSynth")
     initFluidSynth(SoundFontFile)
     time.sleep(7)
-    attachMidiToSynth("20","128")
+    print("Attach Midi Device")
+    if connectMidiToSynth("20","128"):
+        print("connection ok")
+        lightOn()
+    else:
+        print("connection failed")
+        disconnectMidiSynth()
+        stopFluidSynth()
+        lightBlink(3)
+    print("Done")
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(AiyBtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(AiyLedPin, GPIO.OUT)
 
+lightBlink(2)
+
+#onMidiAttachBtn(0)
+GPIO.add_event_detect(AiyBtnPin, GPIO.FALLING, callback=onMidiAttachBtn, bouncetime=12000)
+
 while True:
-    if GPIO.input(AiyBtnPin) == False:
-        print("Button")
-        GPIO.output(AiyLedPin, True)
-        time.sleep(1)
-    else:
-        GPIO.output(AiyLedPin, False)
+        time.sleep(0.1)
